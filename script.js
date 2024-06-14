06edb9ad07b113c5cd93803f51ab65bcc1206d82
Yui Jensen <kyanyui67@gmail.com>
2024-06-14 22:41:44 +0200

    added responsive design

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const forecastTitle = document.querySelector("h2");
 const landingIcon = document.querySelector(".landing-icon");
 const inputBtnContainer = document.querySelector(".input-btn-container");
 const weatherContainer = document.querySelector(".weather-container");
+const currentDate = document.querySelector(".date");
+const place = document.querySelector(".place");
+const weatherImg = document.querySelector(".weather-img");
+const description = document.querySelector(".description");
+const temp = document.querySelector(".temp");
+const wind = document.querySelector(".wind");
+const humidity = document.querySelector(".humidity");
 const errorMessage = document.querySelector(".error-container");
 const weeklyContainer = document.querySelector(".weekly-container");
 const weeklyDates = document.querySelectorAll(".weekly-date");
@@ -54,7 +61,7 @@ async function weeklyWeather(lat, lon) {
 
         weeklyImg[i].src = iconUrl;
         weeklyDates[i].innerHTML = newOptions;
-        weeklyContainer.style.display = "flex";
+        weeklyContainer.style.display = "grid";
       }
     } else {
       console.log(error.message);
@@ -75,38 +82,41 @@ async function weatherApi(city) {
     }
 
     const weatherData = await response.json();
-    weatherContainer.innerHTML = "";
+    //weatherContainer.innerHTML = "";
     //Spell error
     if (weatherData.cod === `404`) {
       errorMessage.style.display = "flex";
       weeklyContainer.style.display = "none";
+      weatherContainer.style.display = "none";
       return;
     }
-    const currentDate = document.createElement("h3");
-    currentDate.innerText = `${monthNames[month]} ${date} `;
-    const location = document.createElement("h1");
-    location.innerText = `${weatherData.name}`;
-    const temp = document.createElement("h3");
-    temp.innerText = `${Math.round(
+    //const currentDate = document.createElement("h3");
+    //const location = document.createElement("h1");
+    //location.style.fontSize = "60px"
+    //const temp = document.createElement("h3");
+    //const weatherState = document.createElement("h3");
+    //const weatherIcon = document.createElement("img");
+    // weatherContainer.appendChild(currentDate);
+    // weatherContainer.appendChild(place);
+    // weatherContainer.appendChild(weatherImg);
+    // weatherContainer.appendChild(temp);
+    // weatherContainer.appendChild(description);
+
+    temp.innerHTML = `${Math.round(
       weatherData.main.temp - 273.15
     )} ℃ / ${Math.round(1.8 * (weatherData.main.temp - 273) + 32)} °F`;
-    const weatherState = document.createElement("h3");
-    weatherState.innerText = `${weatherData.weather[0].description}`;
-    const weatherIcon = document.createElement("img");
+
     const iconCode = `${weatherData.weather[0].icon}`;
     const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
-
-    weatherIcon.src = iconUrl;
-    weatherContainer.appendChild(currentDate);
-    weatherContainer.appendChild(location);
-    weatherContainer.appendChild(weatherIcon);
-    weatherContainer.appendChild(temp);
-    weatherContainer.appendChild(weatherState);
-
-    console.log(weatherData);
-
+    weatherImg.src = iconUrl;
+    currentDate.innerHTML = `${monthNames[month]} ${date} `;
+    place.innerText = `${weatherData.name}`;
+    description.innerText = `${weatherData.weather[0].description}`;
+    humidity.innerText = `Humidity: ${weatherData.main.humidity} %`;
+    wind.innerText = `Wind: ${weatherData.wind.speed}km/h`;
+    weatherContainer.style.display = "flex";
     weeklyWeather(weatherData.coord.lat, weatherData.coord.lon);
-
+    console.log(weatherData);
     // No enter any words
   } catch (error) {
     errorMessage.style.display = "flex";
